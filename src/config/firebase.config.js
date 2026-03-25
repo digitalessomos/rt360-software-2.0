0dcb186d84c93738be7621fccb2d275b4540e80c
Noxie <cristiandarioromero0087@gmail.com>
2026-03-25 18:29:25 -0300

    say hi 3

---
+++ src/config/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+
+const firebaseConfig = {
+     apiKey: "AIzaSyAC0K_7GdPfZ1idnpa0VkwgZ9zBYOB4PRk",
+  authDomain: "crudfbtest.firebaseapp.com",
+  projectId: "crudfbtest",
+  storageBucket: "crudfbtest.firebasestorage.app",
+  messagingSenderId: "171088113288",
+  appId: "1:171088113288:web:eb8b6e333edce9be90b93d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
